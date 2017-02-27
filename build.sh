b542dc97aca3deb7e18b0c6e2df3403d41299a91
Jeremiah Bushau <exjerlock@hotmail.com>
2017-02-27 17:16:04 -0500

    Update build

--- build.sh
+++ build.sh
@@ -1,17 +1,23 @@
 #!/usr/bin/env bash
 
+build() {
+  mkdir build/
+  mkdir build/style
+  mkdir build/style/css
+
+  cp -avr images/ build/images/ 
+  cp -avr scripts/ build/scripts/ 
+  cp -avr style/vendor/ build/style/vendor/ 
+  cp -avr style/fonts/ build/style/fonts/
+  cp -av style/css/main.css build/style/css/main.css 
+  cp -av index.html build/index.html
+}
+
 if [ -e build/ ] 
 then
   rm -rv build/ 
+  build
+else
+  build
 fi 
 
-mkdir build/
-mkdir build/style
-mkdir build/style/css
-
-cp -avr images/ build/images/ 
-cp -avr scripts/ build/scripts/ 
-cp -avr style/vendor/ build/style/vendor/ 
-cp -avr style/fonts/ build/style/fonts/
-cp -av style/css/main.css build/style/css/main.css 
-cp -av index.html build/index.html
