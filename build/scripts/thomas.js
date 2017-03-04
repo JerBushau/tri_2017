1856375094dba4391f4d24a81f1abf053cb50d3a
Jeremiah Bushau <exjerlock@hotmail.com>
2017-03-04 08:35:29 -0500

    Implemented sub-nav into sections

--- build/scripts/thomas.js
+++ build/scripts/thomas.js
@@ -1,34 +1,47 @@
 'use strict'
 
-$('nav').on('click', linkHandler);
+$('nav a').on('click', navHandler);
+$('.sub-nav a').on('click', function (e) {
+  var currentSection = e.target.parentElement.parentElement;
+  if (e.target.className === 'active') {
+    return 
+  }
+  
+  $(currentSection).fadeOut(700);
+})
 
 $('.exit').on('click', exitHandler);
 
-function linkHandler(e) {
+function navHandler(e) {
   var linkText = e.target.textContent;
   var $serviceOverlay = $('section.service');
-  var $installOverlay = $('section.install');
+  var $installOverlay = $('section.installation');
   var $contactOverlay = $('section.contact');
 
   e.preventDefault();
   
+  if (e.target.className === 'active') {
+    return 
+  }
+
   if (linkText === 'Service') {
     
     // css & hide used to fadeIn flex instead of block
-    $serviceOverlay.css("display", "flex").hide().fadeIn();
+    $serviceOverlay.css("display", "flex").hide().fadeIn(700);
 
   } else if (linkText === 'Installation') {
 
-    $installOverlay.css("display", "flex").hide().fadeIn();
+    $installOverlay.css("display", "flex").hide().fadeIn(700);
 
   } else if (linkText === 'Contact') {
 
-    $contactOverlay.css("display", "flex").hide().fadeIn();
+    $contactOverlay.css("display", "flex").hide().fadeIn(700);
   }
 }
 
 function exitHandler(e) {
-  var parent = $(e.target.parentElement);
-  
-  parent.fadeOut();
+  var target = e.target;
+  var currentSection = target.parentElement;
+
+  $(currentSection).fadeOut();
 }
