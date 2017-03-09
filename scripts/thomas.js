0ac8d5b8e4d0e65d144054e979e5245970b349d5
Jeremiah Bushau <exjerlock@hotmail.com>
2017-03-09 13:50:32 -0500

    Implemented back to top button

--- scripts/thomas.js
+++ scripts/thomas.js
@@ -4,11 +4,25 @@
 $('.main-nav').sticky({topSpacing: 0});
 
 // Smooth scroll to anchors
-$('.scroll').on('click', function(evt) {     
-  evt.preventDefault();
-  $('html,body').animate({scrollTop:$(this.hash).offset().top - 54}, 400);
+$('.scroll').on('click', function(e) {     
+  e.preventDefault();
+  $('html,body').animate({scrollTop:$(this.hash).offset().top - 34}, 400);
 
+  $(e.target).blur()
+});
 
-  $(evt.target).blur()
+// Show/hide back to top button
+$('.main-nav').on('sticky-start', function() {
+  $('.b2t').css('display', 'inline-block');
+});
 
+$('.main-nav').on('sticky-end', function() {
+  $('.b2t').css('display', 'none');
+});
+
+// Smooth scroll back to top 
+$('.b2t').on('click', function(e) {
+  $("html, body").animate({scrollTop: 0}, 500);
+
+  $(e.target).blur()
 });
